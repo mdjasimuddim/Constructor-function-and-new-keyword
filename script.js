2631e5aac040e87e42d62e8108dd1178b9cc1964
jasim <mdjasimuddimruet@gmail.com>
2023-03-27 01:38:41 +0600

    Function constructor and new keyword

---
+++ script.js
@@ -0,0 +1,73 @@
+//constructor function
+
+// function Person(name,age,job)  //this is convention
+// {
+//     this.name = name,
+//     this.age = age,
+//     this.job = job
+// }
+
+// const person1 = new Person('md jasim uddim', 25, 'student');
+// console.log(person1.job);
+// console.log(person1.name);
+// console.log(person1.age);
+
+
+// const person2 = new Person('liton hossain', 26, 'web analytics');
+// const person3 = new Person('liton hossain', 26, 'web analytics');
+// const person4 = new Person('kausik debnath', 26, 'data scientist');
+// const person5 = new Person('abm siddiqur rahman', 25, 'software developer');
+// console.log(person2.job);
+// console.log(person2.name);
+// console.log(person2.age);
+
+
+// console.log(person5.name);
+// console.log(person5.job);
+
+
+// const Man = function(name, color, age, height)
+// {
+//     this.name  = name;
+//     this.color = color;
+//     this.age = age,
+//     this.height = height;
+//     this.detailsBio = function (){
+//         console.log(`My name is ${this.name} and my body color is ${this.color} and i am ${this.age} years old and my height is ${this.height}`)
+//     }
+// }
+
+// const Man1 = new Man('Liton hossain', 'white', 26, 5.6);
+// Man1.detailsBio();
+// console.log(Man1.name);
+// console.log(Man1.age);
+
+
+
+// const Man = function(nameArg, colorArg, ageArg, heightArg)
+// {
+//     this.name  = name;
+//     this.color = color;
+//     this.age = age,
+//     this.height = height;
+//     this.detailsBio = function (){
+//         console.log(`My name is ${this.name} and my body color is ${this.color} and i am ${this.age} years old and my height is ${this.height}`)
+//     }
+// }
+
+
+// const Woman = function(nameArg, colorArg, ageArg, heightArg) // this is the best way to declare
+// {
+//     this.name  = nameArg;
+//     this.color = colorArg;
+//     this.age = ageArg,
+//     this.height = heightArg;
+//     this.detailsBio = function (){
+//         console.log(`My name is ${this.name} and my body color is ${this.color} and i am ${this.age} years old and my height is ${this.height}`)
+//     }
+// }
+
+// const woman1 = new Woman('Ritu Khatun', 'black', 22, 5.2);
+// console.log(woman1.name);
+// console.log(woman1.color);
+// console.log(woman1.age);
